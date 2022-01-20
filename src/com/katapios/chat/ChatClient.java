c8c57a5f7185209f28e145dc3d06535f482d4c7f
Dennis Ryumin <d.ryumin@sigma-it.ru>
2022-01-20 11:37:47 +0300

    chat server has been added

--- src/com/katapios/chat/ChatClient.java
+++ src/com/katapios/chat/ChatClient.java
@@ -1,9 +1,12 @@
 package com.katapios.chat;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
+import java.io.*;
+import java.net.Socket;
+
 
 public class ChatClient {
 
@@ -11,6 +14,27 @@ public class ChatClient {
     private JTextField input;
     private JButton sendButton;
     private JButton quitButton;
+
+    private Socket connection = null;
+    private BufferedReader serverIn = null;
+    private PrintStream serverOut = null;
+
+    private void doConnect(){
+        String serverIp = System.getProperty("serverIp","127.0.0.1");
+        String serverPort = System.getProperty("serverPort","2000");
+        try {
+            connection = new Socket(serverIp,Integer.parseInt(serverPort));
+            InputStream is = connection.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            serverIn = new BufferedReader(isr);
+            serverOut = new PrintStream(connection.getOutputStream());
+            Thread t = new Thread(new RemoteReader());
+            t.start();
+        } catch (Exception e) {
+            System.err.println("ERROR: unable to connect to server");
+            e.printStackTrace();
+        }
+    }
 
     public ChatClient() {
         this.output = new JTextArea(10, 50);
@@ -45,6 +69,8 @@ public class ChatClient {
         frame.setAlwaysOnTop(true);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        doConnect();
     }
     
     public static void main(String[] args) {
@@ -57,9 +83,25 @@ public class ChatClient {
         @Override
         public void actionPerformed(ActionEvent e) {
             String message = input.getText();
-            output.append(message+"\n");
+            //output.append(message+"\n");
+            serverOut.print("New message: " + message + "\n");
             input.setText("");
         }
     }
 
+    private class RemoteReader implements Runnable {
+        @Override
+        public void run() {
+
+            try {
+                while (true) {
+                    String nextLine = serverIn.readLine();
+                    output.append(nextLine + "\n");
+                }
+            } catch (Exception e) {
+                System.err.print("ERROR: can't read from the server");
+                e.printStackTrace();
+            }
+        }
+    }
 }
